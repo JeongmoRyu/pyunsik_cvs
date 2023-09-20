92fbaa773d6472b1aa8e9afe91bcfe531c1575b8
flowerdonk <dbwls5140@gmail.com>
2023-09-20 16:36:07 +0900

    chore: 파일 구조 변경

--- backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/kafka/config/CustomSerializer.java
+++ backend/NotificationConsumer/src/main/java/com/picky/notificationconsumer/kafka/config/CustomSerializer.java
@@ -1,4 +1,4 @@
-package com.picky.notificationconsumer.config;
+package com.picky.notificationconsumer.kafka.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
