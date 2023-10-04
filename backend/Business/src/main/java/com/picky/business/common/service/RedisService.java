af91c1e8f5fd8d00912d88aea62d1660ff9f95c8
Dohyun822 <yup8229@gmail.com>
2023-10-05 08:18:04 +0900

    feature : redis 데이터 읽기 위한 service 클래스

--- backend/Business/src/main/java/com/picky/business/common/service/RedisService.java
+++ backend/Business/src/main/java/com/picky/business/common/service/RedisService.java
@@ -8,23 +8,26 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
+
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class RedisService  {
-    private StringRedisTemplate stringRedisTemplate;
+public class RedisService {
+
+    private final StringRedisTemplate stringRedisTemplate;
 
     //인기검색어 가져오기
     public Map<String, List<Map<String, String>>> getKeywordRanking() {
         String jsonStr = stringRedisTemplate.opsForValue().get("realtime_search_keywords");
         ObjectMapper objectMapper = new ObjectMapper();
 
-        Map<String, List<Map<String, String>>> result = null;
+        Map<String, List<Map<String, String>>> result;
         try {
-            result = objectMapper.readValue(jsonStr, new TypeReference<Map<String, List<Map<String, String>>>>() {});
+            result = objectMapper.readValue(jsonStr, new TypeReference<>() {
+            });
         } catch (JsonProcessingException e) {
             e.printStackTrace();
             throw new NotFoundException("redis에 저장된 값이 없습니다.");
@@ -43,10 +46,11 @@ public class RedisService  {
         }
 
         ObjectMapper objectMapper = new ObjectMapper();
-        List<Long> productIds = null;
+        List<Long> productIds;
 
         try {
-            productIds = objectMapper.readValue(jsonStr, new TypeReference<List<Long>>() {});
+            productIds = objectMapper.readValue(jsonStr, new TypeReference<>() {
+            });
         } catch (JsonProcessingException e) {
             e.printStackTrace();
             throw new NotFoundException("데이터 처리 오류");
